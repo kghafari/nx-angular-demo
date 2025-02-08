4d288f831052fc38950e469a4089add8132b5350
Kyle Ghafari <kyleghafari@gmail.com>
2025-02-08 17:13:20 -0500

    refactor game service. add some more test data

--- apps/nx-angular-demo/src/app/modules/tab/tab.component.ts
+++ apps/nx-angular-demo/src/app/modules/tab/tab.component.ts
@@ -3,13 +3,13 @@ import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { GameStateService } from '../idle/services/game-state.service';
 import { MatTabsModule } from '@angular/material/tabs';
 import { PianoRollV2Component } from '@nx-angular-demo/piano-roll';
 import { MinutesSecondsPipe } from '@nx-angular-demo/pipes';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { map, throttleTime } from 'rxjs';
-import { Resources, ResourceType } from '../idle/models/idle.interfaces';
+import { Resource, Resources, ResourceType } from '../idle/models/idle.interfaces';
+import { GameStateService } from '../idle/services/game-state.service';
 
 @Component({
   selector: 'app-tab',
@@ -43,26 +43,27 @@ export class TabComponent {
     })
   );
 
-  protected updateResourceMult(resource: ResourceType, value: number): void {
+  protected updateResourceMult(resource: Resource, value: number): void {
     console.log('updateResourceMult', resource, value);
     this.gameService.applyUpgrade(resource, value);
   }
 
-  private normalizeResources(resources: Resources): Resources {
-    const maxVal = Math.max(...Object.values(resources));
+  private normalizeResources(resources: Resource[]): Resource[]{
+    //const maxVal = Math.max(...Object.values(resources));
+
+    const maxVal = Math.max(...resources.map((r) => r.value));
 
     // avoid division by zero
     if (maxVal === 0) {
       return resources;
     }
 
-    return {
-      production: (resources.production / maxVal) * 100,
-      science: (resources.science / maxVal) * 100,
-      faith: (resources.faith / maxVal) * 100,
-      military: (resources.military / maxVal) * 100,
-      culture: (resources.culture / maxVal) * 100,
-    };
+    return resources.map((r) => {
+      return {
+        ...r,
+        value: r.value / maxVal * 100,
+      };
+    });
   }
 
   trackByKey(index: number, item: { key: string; value: number }): string {
